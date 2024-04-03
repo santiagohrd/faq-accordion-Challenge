a64fa06688607e169a78dfe8f930f4e9ea76f226
SantiagoHrd <santiagohrd1@gmail.com>
2024-04-03 12:16:54 -0500

    Add main.js: Create an iteration to return each question and answer into a new html element, and join them to their parent

--- main.js
+++ main.js
@@ -4,3 +4,18 @@ const faqs = {
     'Can I use Frontend Mentor projects in my portfolio?': 'Yes, you can use projects completed on Frontend Mentor in your portfolio. It\'s an excellentway to showcase your skills to potential employers!',
     'How can I get help if I\'m stuck on a Frontend Mentor challenge?': 'The best place to get help is inside Frontend Mentor\'s Discord community. There\'s a help channel where you can ask questions and seek support from other community members.'
 }
+
+const faqContainer = document.createElement('div');
+
+for (const [question, answer] of Object.entries(faqs)) {
+    const questionTitle = document.createElement('h3');
+    const questionAnswer = document.createElement('p');
+
+    questionTitle.textContent = question;
+    questionAnswer.textContent = answer;
+
+    faqContainer.appendChild(questionTitle);
+    faqContainer.appendChild(questionAnswer);
+}
+
+document.body.appendChild(faqContainer);
