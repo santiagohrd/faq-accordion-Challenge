5d3bec183a59c8f15071b248a3864abce443ee55
SantiagoHrd <santiagohrd1@gmail.com>
2024-04-03 13:44:00 -0500

    Fix main.js: Change the iterable to make sure only return the question

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const faqs = {
-    'What is Frontend Mentor, and how will it help me?': 'Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It\'s suitable for all levels and ideal for portfolio building.',
+    '   What is Frontend Mentor, and how will it help me?': 'Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It\'s suitable for all levels and ideal for portfolio building.',
     'Is Frontend Mentor free?': 'Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels.',
     'Can I use Frontend Mentor projects in my portfolio?': 'Yes, you can use projects completed on Frontend Mentor in your portfolio. It\'s an excellentway to showcase your skills to potential employers!',
     'How can I get help if I\'m stuck on a Frontend Mentor challenge?': 'The best place to get help is inside Frontend Mentor\'s Discord community. There\'s a help channel where you can ask questions and seek support from other community members.'
@@ -7,15 +7,21 @@ const faqs = {
 
 const faqContainer = document.createElement('div');
 
-for (const [question, answer] of Object.entries(faqs)) {
+for (question of Object.keys(faqs)) {
     const questionTitle = document.createElement('h3');
-    const questionAnswer = document.createElement('p');
 
     questionTitle.textContent = question;
-    questionAnswer.textContent = answer;
-
     faqContainer.appendChild(questionTitle);
-    faqContainer.appendChild(questionAnswer);
 }
 
 document.body.appendChild(faqContainer);
+
+function openedQuestion(){
+    const iconMinus = document.createElement('img');
+    iconPlus.src = '/assets/images/icon-minus.svg';
+}
+
+function closedQuestion(){
+    const iconPlus = document.createElement('img');
+    iconPlus.src = '/assets/images/icon-plus.svg';
+}
